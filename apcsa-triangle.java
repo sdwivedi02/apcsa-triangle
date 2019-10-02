f244164b443484d62251b96325d91b4357c64502
Sambhavi Dwivedi <ssambhavi02@gmail.com>
2019-10-02 14:35:23 -0400

    third commit

--- apcsa-triangle.java
+++ apcsa-triangle.java
@@ -1,5 +1,21 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class Triangle {
 public static void main(String[] args) {
-  
+
+  Scanner in = new Scanner(System.in);
+
+  int height;
+  int base;
+  int area;
+
+  System.out.print("Enter Height: ");
+  int height = in.nextLine();
+
+  System.out.print("Enter Base: ");
+  int base = in.nextLine();
+
+  area = (base * height)/2;
+   System.out.println("The area of a triangle with a height and base of "+height+" and "+base+" is "+area);
 }
 }
